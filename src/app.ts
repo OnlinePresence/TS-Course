c169eddfc35d855fcc08455da8b745139fd08752
kkluska <kkluska@fandom.com>
2023-10-30 09:54:14 +0100

    TS Course Section 4 Setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,1 @@
-const button = document.querySelector('button')!;
-
-
-function clickHandler(message: string) {
-    console.log('Clicked! ' + message);
-}
-//! is used when you know for sure the value won't be empty
-// if you're less sure you use if statement
-if (button) {
-button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
-}
+// Code goes here!
